8fdc1864272a077e69f163e6c4c780ab3f518f76
lishaopeng <610031525@qq.com>
2022-05-30 14:13:31 +0800

    更新

--- src/get_hn_major_score_line.py
+++ src/get_hn_major_score_line.py
@@ -1,3 +1,5 @@
+import time
+
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.actions import interaction
@@ -9,7 +11,6 @@ from appium import webdriver
 from mylib import *
 import db
 from src.base import Base
-from mylib import *
 
 
 class HnMajorScoreLine(Base):
@@ -18,7 +19,7 @@ class HnMajorScoreLine(Base):
         driver = self.driver
 
         time.sleep(10)
-        find_college_element = driver.find_element(by=AppiumBy.ID, value="com.eagersoft.youzy.youzy:id/ll_btn_zdx")
+        find_college_element = driver.find_element(by=AppiumBy.ID, value="com.eagersoft.youzy.youzy:id/iv_logo")
         find_college_element.click()
         # 点击查所有大学
         find_all_college_element = driver.find_element(by=AppiumBy.ID,
@@ -27,12 +28,11 @@ class HnMajorScoreLine(Base):
         time.sleep(3)
         my_db = db.DataBase()
         cursor = my_db.get_cursor()
-        colleges = get_all_colleges(cursor, province="湖南", data_type="major_score_line")
-        no_need_colleges = get_all_colleges_of_no_need(cursor, province="湖南", data_type="major_score_line")
-        for i in range(0,190):
+        colleges = get_all_colleges_hn(cursor, province="湖南")
+        for i in range(0, 14):
             self.next_page()
         for i in range(0, 500):
-            self.get_one_page_major_score_line(colleges, cursor, no_need_colleges)
+            self.get_one_page_major_score_line(colleges)
             self.next_page()
 
         self.driver.quit()
@@ -66,10 +66,8 @@ class HnMajorScoreLine(Base):
                 # 最后需要关闭招生方向的toast
                 driver.find_element(by=AppiumBy.ID, value="com.eagersoft.youzy.youzy:id/iv_close").click()
 
-    def get_one_page_major_score_line(self, colleges, cursor, no_need_colleges):
+    def get_one_page_major_score_line(self, colleges):
         driver = self.driver
-        province = "湖南"
-        data_type = "major_score_line"
         college_list_element_count = len(
             list(driver.find_elements(by=AppiumBy.ID, value="com.eagersoft.youzy.youzy:id/menu")))
         # print(driver.page_source)
@@ -80,18 +78,19 @@ class HnMajorScoreLine(Base):
             else:
                 college = college_element.find_element(by=AppiumBy.ID, value="com.eagersoft.youzy.youzy:id/name").text
                 college_name = college.split(" ")[0]
-                # 如果college_name 在不需要的colleges 或者在已经爬取的college 或者在self的college_list就不需要爬取了
-                if college_name in no_need_colleges or college_name in colleges or college_name in self.college_list:
+                if college_name in colleges:
                     continue
-
-                self.college_list.append(college_name)
+                if college_name not in self.college_list:
+                    self.college_list.append(college_name)
+                else:
+                    continue
                 college_element.click()
             el15 = driver.find_element(by=AppiumBy.ID, value="com.eagersoft.youzy.youzy:id/click_history")
             el15.click()
             # 点击专业分数线tab
 
             title_college_name = driver.find_element(by=AppiumBy.ID, value="com.eagersoft.youzy.youzy:id/title").text
-            if title_college_name in no_need_colleges or title_college_name in colleges or title_college_name in self.college_list:
+            if title_college_name in colleges:
                 # 点击从专业分数线回退
                 back_element = driver.find_element(by=AppiumBy.ID, value="com.eagersoft.youzy.youzy:id/leftBackImg")
                 back_element.click()
@@ -99,15 +98,13 @@ class HnMajorScoreLine(Base):
                 back_element = driver.find_element(by=AppiumBy.ID, value="com.eagersoft.youzy.youzy:id/click_back")
                 back_element.click()
             else:
-                # 把college_name 写进数据库
-                insert_data_to_yzy_college(cursor, title_college_name, province, data_type)
                 driver.tap([(540, 238), ])
                 # 点击招生方向,先判断有没有招生方向
                 # 默认
                 grade = "本科"
                 science_art = "历史"
                 time.sleep(2)
-                if not is_element_present(self.driver,
+                if not is_element_present(self,
                                                      "com.eagersoft.youzy.youzy:id/material_spinner_year"):
                     # 如果这里没加载出来，直接返回
                     # 点击从专业分数线回退
